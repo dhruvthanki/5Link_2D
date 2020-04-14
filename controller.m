0ecea250f408957814c0df9861b66ab98aaf6049
Thanki Dhruv Ashwinkumar <31178720+dhruvthanki@users.noreply.github.com>
2020-04-13 20:37:48 -0400

    Added PD tracking

--- controller.m
+++ controller.m
@@ -1,5 +1,9 @@
 function [u,fx,gx] = controller(x,a,thm,thp)
 
+e = 0.1;
+Kp = (16/(e^2))*eye(4);
+Kd = (8/e)*eye(4);
+
 dq = [x(6); x(7); x(8); x(9); x(10)];
 D = D_q(x);
 G = G_q(x);
@@ -13,6 +17,7 @@ lfh = Lfh(x,a,thm,thp);
 l2fh = JacLfh(x,a,thm,thp)*fx;
 lglfh = JacLfh(x,a,thm,thp)*gx;
 h = h_q(x,a,thm,thp);
+v = -Kp*h - Kd*lfh;
+% v=PSIi(h,lfh,0.9,0.1);
 
-v=PSIi(h,lfh,0.9,0.1);
 u = lglfh\(v - l2fh);
